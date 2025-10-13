6c351a68963bace5fb8fdb0d0de031d380877fc4
martinolee <soohan.m.lee@gmail.com>
2025-10-13 17:19:14 +0900

    add default project

---
+++ Link/Link/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Link
+//
+//  Created by Soohan Lee on 10/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    EmptyView()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ Link/Link/LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkApp.swift
+//  Link
+//
+//  Created by Soohan Lee on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct LinkApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
